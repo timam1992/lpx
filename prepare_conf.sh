e43956e81d3b5476fb99a97eaf8bac17cec90bfc
Timam <me@timam.io>
2018-03-20 15:59:34 +0600

    Implemented functions

--- prepare_conf.sh
+++ prepare_conf.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
+take_backup(){
+    #Creating backup of /etc/hosts file
+    cat /etc/hosts > hosts.backup
+}
 
-echo -e 'events {' >> nginx.conf
-echo -e ' \t ' 'worker_connections  1024;' >> nginx.conf
-echo -e '}\n' >> nginx.conf
+write_nginx_header(){
+    echo -e 'events {' >> nginx.conf
+    echo -e ' \t ' 'worker_connections  1024;' >> nginx.conf
+    echo -e '}\n' >> nginx.conf
+}
 
+prepare_nginx_instance(){
+    printf 'Enter your desired local domain : '
+    read -r domain
+    ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+    printf 'Your local IP address is :'$ip'\n'
+    printf 'Enter the Port Number : '
+    read -r port 
 
-printf 'Enter your desired local domain : '
-read -r domain
-ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-printf 'Your local IP address is :'$ip'\n'
-printf 'Enter the Port Number : '
-read -r port 
+    echo 'http {' >> nginx.conf
+    echo -e ' \t ' 'server {' >> nginx.conf
+    echo -e ' \t \t' 'listen       80;' >> nginx.conf
+    echo -e ' \t \t' 'server_name' "$domain" ';\n' >> nginx.conf
+    echo -e ' \t \t' 'location / {' >> nginx.conf
+    echo -e ' \t \t \t' 'proxy_pass http://'"$ip"':'"$port"';' >> nginx.conf
+    echo -e ' \t \t' '}' >> nginx.conf
+    echo -e ' \t' '}' >> nginx.conf
+    echo '}' >> nginx.conf
 
+    #updating hosts file 
+    echo -e "$ip"  '\t'   "$domain" >> /etc/hosts
+}
 
-
-echo 'http {' >> nginx.conf
-echo -e ' \t ' 'server {' >> nginx.conf
-echo -e ' \t \t' 'listen       80;' >> nginx.conf
-echo -e ' \t \t' 'server_name' "$domain" ';\n' >> nginx.conf
-echo -e ' \t \t' 'location / {' >> nginx.conf
-echo -e ' \t \t \t' 'proxy_pass http://'"$ip"':'"$port"';' >> nginx.conf
-echo -e ' \t \t' '}' >> nginx.conf
-echo -e ' \t' '}' >> nginx.conf
-echo '}' >> nginx.conf
-
-#Creating backup of /etc/hosts file
-cat /etc/hosts > hosts.backup
-#updating hosts file 
-echo -e "$ip"  '\t'   "$domain" >> /etc/hosts
+take_backup
+write_nginx_header
+prepare_nginx_instance
